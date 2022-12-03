0c81bd9dae2d8fd59ad09881563aa391770eead7
Masaru Tsuchiyama <m.tmatma@gmail.com>
2022-12-03 20:11:02 +0900

    echo on

--- ci_scripts/build_appveyor.bat
+++ ci_scripts/build_appveyor.bat
@@ -1,3 +1,4 @@
+echo on
 if exist teraterm\ttpdlg\svnversion.h del teraterm\ttpdlg\svnversion.h
 if exist buildtools\svnrev\sourcetree_info.bat del buildtools\svnrev\sourcetree_info.bat
 call ci_scripts\install.bat
