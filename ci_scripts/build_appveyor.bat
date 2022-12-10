7706a81b08d870a666cd79ed3cbfcb1cab7d65c2
Masaru Tsuchiyama <m.tmatma@gmail.com>
2022-12-11 08:26:48 +0900

    add log

--- ci_scripts/build_appveyor.bat
+++ ci_scripts/build_appveyor.bat
@@ -1,4 +1,5 @@
 echo on
+echo ------------------- APPVEYOR 0 -----------------------------------
 if exist teraterm\ttpdlg\svnversion.h del teraterm\ttpdlg\svnversion.h
 if exist buildtools\svnrev\sourcetree_info.bat del buildtools\svnrev\sourcetree_info.bat
 echo ------------------- APPVEYOR 1 -----------------------------------
