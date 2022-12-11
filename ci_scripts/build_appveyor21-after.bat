008cd051372479a6b48da6ebb9c5557593c5dd83
Masaru Tsuchiyama <m.tmatma@gmail.com>
2022-12-11 13:07:04 +0900

    remove timestamp

--- ci_scripts/build_appveyor21-after.bat
+++ ci_scripts/build_appveyor21-after.bat
@@ -1,7 +1,7 @@
 echo on
 echo ------------------- APPVEYOR 21 after------------------------------
 cd %BUILD_DIR%
-set ZIP_FILE=snapshot-%VERSION%-r%SVNVERSION%-%DATE%_%TIME%-appveyor-%COMPILER_FRIENDLY%.zip
-set SETUP_FILE=snapshot-%VERSION%-r%SVNVERSION%-%DATE%_%TIME%-appveyor-%COMPILER_FRIENDLY%
-set SNAPSHOT_DIR=snapshot-r%SVNVERSION%-%DATE%_%TIME%-appveyor-%COMPILER_FRIENDLY%
+set ZIP_FILE=snapshot-%VERSION%-r%SVNVERSION%-appveyor-%COMPILER_FRIENDLY%.zip
+set SETUP_FILE=snapshot-%VERSION%-r%SVNVERSION%-appveyor-%COMPILER_FRIENDLY%
+set SNAPSHOT_DIR=snapshot-r%SVNVERSION%-appveyor-%COMPILER_FRIENDLY%
 dir /b /s /a-d *.exe
