fa52a248500e057e48ce88c3ad7cb3a6010ec33c
Masaru Tsuchiyama <m.tmatma@gmail.com>
2022-12-11 12:32:48 +0900

    fix path

--- ci_scripts/build_appveyor22.bat
+++ ci_scripts/build_appveyor22.bat
@@ -7,8 +7,8 @@ set SNAPSHOT_DIR=snapshot-r%SVNVERSION%-%DATE%_%TIME%-appveyor-%COMPILER_FRIENDL
 
 mkdir %~dp0\..\cygwin\cygterm\cygterm+-i686
 mkdir %~dp0\..\cygwin\cygterm\cygterm+-x86_64
-copy /Y Release\cygterm.exe                                                  %~dp0\..\cygwin\cygterm\cygterm+-i686\cygterm.exe
-copy /Y cygwin\cygterm_build\cygterm_build\build_cygterm_x86_64\cygterm.exe  %~dp0\..\cygwin\cygterm\cygterm+-x86_64\cygterm.exe
+copy /Y "Release\cygterm.exe"                                                  "%~dp0\..\cygwin\cygterm\cygterm+-i686\cygterm.exe"
+copy /Y "cygwin\cygterm_build\cygterm_build\build_cygterm_x86_64\cygterm.exe"  "%~dp0\..\cygwin\cygterm\cygterm+-x86_64\cygterm.exe"
 
 echo "%CMAKE_COMMAND%" --build . --target inno_setup
 "%CMAKE_COMMAND%" --build . --target inno_setup                   || exit /b !ERRORLEVEL!
