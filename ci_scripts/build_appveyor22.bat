ba3ef735fa6846439f490352382f5399249d3827
Masaru Tsuchiyama <m.tmatma@gmail.com>
2022-12-11 13:27:12 +0900

    change path

--- ci_scripts/build_appveyor22.bat
+++ ci_scripts/build_appveyor22.bat
@@ -5,10 +5,10 @@ set ZIP_FILE=snapshot-%VERSION%-r%SVNVERSION%-appveyor-%COMPILER_FRIENDLY%.zip
 set SETUP_FILE=snapshot-%VERSION%-r%SVNVERSION%-appveyor-%COMPILER_FRIENDLY%
 set SNAPSHOT_DIR=snapshot-r%SVNVERSION%-appveyor-%COMPILER_FRIENDLY%
 
-mkdir %~dp0\..\cygwin\cygterm\cygterm+-i686
-mkdir %~dp0\..\cygwin\cygterm\cygterm+-x86_64
-copy /Y "Release\cygterm.exe"                                                  "%~dp0\..\cygwin\cygterm\cygterm+-i686\cygterm.exe"
-copy /Y "cygwin\cygterm_build\cygterm_build\build_cygterm_x86_64\cygterm.exe"  "%~dp0\..\cygwin\cygterm\cygterm+-x86_64\cygterm.exe"
+mkdir %SNAPSHOT_DIR%\cygwin\cygterm\cygterm+-i686
+mkdir %SNAPSHOT_DIR%\cygwin\cygterm\cygterm+-x86_64
+copy /Y "Release\cygterm.exe"                                                  "%SNAPSHOT_DIR%\cygwin\cygterm\cygterm+-i686\cygterm.exe"
+copy /Y "cygwin\cygterm_build\cygterm_build\build_cygterm_x86_64\cygterm.exe"  "%SNAPSHOT_DIR%\cygwin\cygterm\cygterm+-x86_64\cygterm.exe"
 
 echo "%CMAKE_COMMAND%" --build . --target inno_setup
 "%CMAKE_COMMAND%" --build . --target inno_setup                   || exit /b !ERRORLEVEL!
