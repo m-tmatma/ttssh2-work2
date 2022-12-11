6a505983b755a591fd68c1d7eeeae4d48c30dd18
Masaru Tsuchiyama <m.tmatma@gmail.com>
2022-12-11 12:21:48 +0900

    fix copy order

--- ci_scripts/build_appveyor22.bat
+++ ci_scripts/build_appveyor22.bat
@@ -4,10 +4,11 @@ cd %BUILD_DIR%
 set ZIP_FILE=snapshot-%VERSION%-r%SVNVERSION%-%DATE%_%TIME%-appveyor-%COMPILER_FRIENDLY%.zip
 set SETUP_FILE=snapshot-%VERSION%-r%SVNVERSION%-%DATE%_%TIME%-appveyor-%COMPILER_FRIENDLY%
 set SNAPSHOT_DIR=snapshot-r%SVNVERSION%-%DATE%_%TIME%-appveyor-%COMPILER_FRIENDLY%
-echo "%CMAKE_COMMAND%" --build . --target inno_setup
-"%CMAKE_COMMAND%" --build . --target inno_setup                   || exit /b !ERRORLEVEL!
 
 mkdir %~dp0\..\cygwin\cygterm\cygterm+-i686
 mkdir %~dp0\..\cygwin\cygterm\cygterm+-x86_64
 copy /Y Release\cygterm.exe                                                  %~dp0\..\cygwin\cygterm\cygterm+-i686\cygterm.exe
 copy /Y cygwin\cygterm_build\cygterm_build\build_cygterm_x86_64\cygterm.exe  %~dp0\..\cygwin\cygterm\cygterm+-x86_64\cygterm.exe
+
+echo "%CMAKE_COMMAND%" --build . --target inno_setup
+"%CMAKE_COMMAND%" --build . --target inno_setup                   || exit /b !ERRORLEVEL!
