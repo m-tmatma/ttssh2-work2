8fdafa27927b0931dfd24a32889ece9a3c32cb6c
Masaru Tsuchiyama <m.tmatma@gmail.com>
2022-12-11 12:06:12 +0900

    add create dir

--- ci_scripts/build_appveyor22.bat
+++ ci_scripts/build_appveyor22.bat
@@ -7,5 +7,7 @@ set SNAPSHOT_DIR=snapshot-r%SVNVERSION%-%DATE%_%TIME%-appveyor-%COMPILER_FRIENDL
 echo "%CMAKE_COMMAND%" --build . --target inno_setup
 "%CMAKE_COMMAND%" --build . --target inno_setup                   || exit /b !ERRORLEVEL!
 
+mkdir %~dp0\..\cygwin\cygterm\cygterm+-i686
+mkdir %~dp0\..\cygwin\cygterm\cygterm+-x86_64
 copy /Y Release\cygterm.exe                                                  %~dp0\..\cygwin\cygterm\cygterm+-i686\cygterm.exe
 copy /Y cygwin\cygterm_build\cygterm_build\build_cygterm_x86_64\cygterm.exe  %~dp0\..\cygwin\cygterm\cygterm+-x86_64\cygterm.exe
