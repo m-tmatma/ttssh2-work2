c0bc5f8eb509526d5bf61b9f916fe25b101098a4
Masaru Tsuchiyama <m.tmatma@gmail.com>
2022-12-11 11:50:05 +0900

    copy cygterm.exe

--- ci_scripts/build_appveyor22.bat
+++ ci_scripts/build_appveyor22.bat
@@ -6,3 +6,6 @@ set SETUP_FILE=snapshot-%VERSION%-r%SVNVERSION%-%DATE%_%TIME%-appveyor-%COMPILER
 set SNAPSHOT_DIR=snapshot-r%SVNVERSION%-%DATE%_%TIME%-appveyor-%COMPILER_FRIENDLY%
 echo "%CMAKE_COMMAND%" --build . --target inno_setup
 "%CMAKE_COMMAND%" --build . --target inno_setup                   || exit /b !ERRORLEVEL!
+
+copy /Y Release\cygterm.exe                                                  %~dp0\..\cygwin\cygterm\cygterm+-i686\cygterm.exe
+copy /Y cygwin\cygterm_build\cygterm_build\build_cygterm_x86_64\cygterm.exe  %~dp0\..\cygwin\cygterm\cygterm+-x86_64\cygterm.exe
