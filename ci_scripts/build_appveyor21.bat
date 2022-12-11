e5b4146a9224c854247e2f059737a2ffb7bff2b1
Masaru Tsuchiyama <m.tmatma@gmail.com>
2022-12-11 13:07:04 +0900

    remove timestamp

--- ci_scripts/build_appveyor21.bat
+++ ci_scripts/build_appveyor21.bat
@@ -1,8 +1,8 @@
 echo on
 echo ------------------- APPVEYOR 21 -----------------------------------
 cd %BUILD_DIR%
-set ZIP_FILE=snapshot-%VERSION%-r%SVNVERSION%-%DATE%_%TIME%-appveyor-%COMPILER_FRIENDLY%.zip
-set SETUP_FILE=snapshot-%VERSION%-r%SVNVERSION%-%DATE%_%TIME%-appveyor-%COMPILER_FRIENDLY%
-set SNAPSHOT_DIR=snapshot-r%SVNVERSION%-%DATE%_%TIME%-appveyor-%COMPILER_FRIENDLY%
+set ZIP_FILE=snapshot-%VERSION%-r%SVNVERSION%-appveyor-%COMPILER_FRIENDLY%.zip
+set SETUP_FILE=snapshot-%VERSION%-r%SVNVERSION%-appveyor-%COMPILER_FRIENDLY%
+set SNAPSHOT_DIR=snapshot-r%SVNVERSION%-appveyor-%COMPILER_FRIENDLY%
 echo "%CMAKE_COMMAND%" --build . --target zip
 "%CMAKE_COMMAND%" --build . --target zip                          || exit /b !ERRORLEVEL!
