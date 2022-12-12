f5fa4880ba34c35217c40ebf0197a7711a23a58f
Masaru Tsuchiyama <m.tmatma@gmail.com>
2022-12-12 16:59:08 +0900

    add copy cygterm.exe

--- ci_scripts/build_appveyor.bat
+++ ci_scripts/build_appveyor.bat
@@ -95,6 +95,11 @@ echo "%CMAKE_COMMAND%" --build . --target zip
 echo ------------------- APPVEYOR 21 after------------------------------
 dir /b /s /a-d *.exe
 echo ------------------- APPVEYOR 22 -----------------------------------
+mkdir %SNAPSHOT_DIR%\cygterm+-i686
+mkdir %SNAPSHOT_DIR%\cygterm+-x86_64
+copy /Y "Release\cygterm.exe"                                                  "%SNAPSHOT_DIR%\cygterm+-i686\cygterm.exe"
+copy /Y "cygwin\cygterm_build\cygterm_build\build_cygterm_x86_64\cygterm.exe"  "%SNAPSHOT_DIR%\cygterm+-x86_64\cygterm.exe"
+
 echo "%CMAKE_COMMAND%" --build . --target inno_setup
 "%CMAKE_COMMAND%" --build . --target inno_setup                   || exit /b !ERRORLEVEL!
 echo ------------------- APPVEYOR 23 -----------------------------------
