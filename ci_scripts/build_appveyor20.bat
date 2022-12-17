61402050821a7980315304651447d1000335cf44
Masaru Tsuchiyama <m.tmatma@gmail.com>
2022-12-17 21:03:29 +0900

    fix

--- ci_scripts/build_appveyor20.bat
+++ ci_scripts/build_appveyor20.bat
@@ -1,8 +1,13 @@
 echo on
 echo ------------------- APPVEYOR 20 -----------------------------------
 cd %BUILD_DIR%
-set ZIP_FILE=snapshot-%VERSION%-r%SVNVERSION%-appveyor-%COMPILER_FRIENDLY%.zip
-set SETUP_FILE=snapshot-%VERSION%-r%SVNVERSION%-appveyor-%COMPILER_FRIENDLY%
-set SNAPSHOT_DIR=snapshot-r%SVNVERSION%-appveyor-%COMPILER_FRIENDLY%
+if "%GITREV%"=="" (
+    set REVISION=%GITREV%
+) else (
+    set REVISION=r%SVNVERSION%
+)
+set ZIP_FILE=snapshot-%VERSION%-%REVISION%-appveyor-%COMPILER_FRIENDLY%.zip
+set SETUP_FILE=snapshot-%VERSION%-%REVISION%-appveyor-%COMPILER_FRIENDLY%
+set SNAPSHOT_DIR=snapshot-%REVISION%-appveyor-%COMPILER_FRIENDLY%
 echo "%CMAKE_COMMAND%" --build . --target install %CMAKE_OPTION_BUILD%
 "%CMAKE_COMMAND%" --build . --target install %CMAKE_OPTION_BUILD% || exit /b !ERRORLEVEL!
